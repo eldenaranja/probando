10a4a40fed39756c98d1824a2e6aa1ac80864367
dam127 <dam127@PC2020.aulas.local>
2016-04-08 12:45:22 +0200

    commit rama

--- src/Persona.java
+++ src/Persona.java
@@ -10,9 +10,11 @@
  */
 public class Persona {
     String nombre;
+    int edad;
     
-    public Persona(String nombre) {
+    public Persona(String nombre,int edad) {
         this.nombre=nombre;
+        this.edad=edad;
     }
 
 }
